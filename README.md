33cd1988ed09bdf73bb42540d4374c1ee79fd97c
Ashish Kumar <ashish.kumar@tadigital.com>
2021-02-02 19:23:30 +0530

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # savior.js
+A JavaScript utility to save base64 data as a file on local disc.
+
+# installation
+`npm install savior.js --save`
+
+# usage
+`import { savior } from 'savior.js';`
---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+/**
+ * @function saveAsFile
+ * @desc saves base64 byte data as file to local disc
+ * @param { string } byteData base64 data to be saved
+ * @param { string } fileName name of the file it has to be saved as
+ * @param { string } type document type
+ */
+function saveAsFile (byteData, fileName, type) {
+    const linkSource = `data:application/${type};base64,${byteData}`;
+    const downloadLink = document.createElement("a");
+    
+    //
+    fileName = fileName || `document.${type}`;
+
+    //
+    downloadLink.href = linkSource;
+    downloadLink.download = fileName;
+    downloadLink.click();
+}
+
+// export
+module.exports.savior = saveAsFile;
